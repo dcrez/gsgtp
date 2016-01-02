63d2b203823e5ef2f68e1bf1b46150a1f9f6245f
D. Creswick <danielle.creswick@gmail.com>
2016-01-02 04:25:31 -0600

    Continue rules' file inquiry

--- js/rules.js
+++ js/rules.js
@@ -1,11 +1,11 @@
-$( "#general" ).load( "assets/rules/general.txt" );
+$( "#general" ).load( "/assets/rules/general.txt" );
 
-$( "#classes" ).load( "assets/rules/classes.txt" );
+$( "#classes" ).load( "/assets/rules/classes.txt" );
 
-$( "#operations" ).load( "assets/rules/operations.txt" );
+$( "#operations" ).load( "/assets/rules/operations.txt" );
 
-$( "#disqualification" ).load( "assets/rules/disqualification.txt" );
+$( "#disqualification" ).load( "/assets/rules/disqualification.txt" );
 
-$( "#penalties" ).load( "assets/rules/penalties.txt" );
+$( "#penalties" ).load( "/assets/rules/penalties.txt" );
 
-$( "#protests" ).load( "assets/rules/protests.txt" );
+$( "#protests" ).load( "/assets/rules/protests.txt" );
