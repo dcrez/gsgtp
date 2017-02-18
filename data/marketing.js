23404dda89275081c03f69c9f3c91a5326a5a2de
D. Creswick <danielle.creswick@gmail.com>
2017-02-18 00:37:36 -0600

    Post changes to dc

---
+++ data/marketing.js
@@ -0,0 +1,44 @@
+var marketing = {
+
+"marketing_content": [
+  {
+    "message": "Donuts",
+    "tagline": "We won't bring you donuts",
+    "tagline-details": "We run lean. No gimmicks. No fuss. Using tech, we minimze our overhead to maximize your payout.",
+    "small-image": "assets/donuts.png",
+    "background-image": "",
+    "detail-heading": "What do you want? Donuts or dollars?",
+    "details": [
+      {"p":"Just so we're clear upfront, if we successfully place you in a project at one of our clients, we’re not going to randomly drop in and bring you donuts."},
+      {"p":"We are also not going to give you STAR-themed swag like pens, notebooks or t-shirts nor do we have a box suite for the Twins, Wild, or Vikings, so don’t look for a game invite from us. It’s not that we don’t care or want to hang with you, it’s just that we thought you’d rather get more money in your pocket from the bill rate on the project, and isn’t that a lot more exciting than a bunch of junk food or chachkies that will collect dust on your shelf?"},
+      {"p":"Moreover, STAR does not offer bloated overhead costs like fancy offices, multiple levels of management, or highly-compensated sales people. All of these things add up and take money out of your pocket. We run a lean operation that ensures our clients get talent at market rates, while giving you, the consultant, the bulk of the bill rate. Our low and transparent markup ensures that without all this unnecessary fluff in the system, you are getting the best rate possible on consulting gigs through STAR."}
+    ]
+    },
+    {
+      "message": "Rabbits",
+      "tagline": "We let the rabbit out of the hat",
+      "tagline-details": "No secrets. No surprises. We let you know the req and bill rate, and get your permission before submitting for a role.",
+      "small-image": "assets/rabbit.png",
+      "background-image": "",
+      "detail-heading": "We know you can’t work with just one staffing and consulting firm. We just want to be your favorite.",
+      "details": [
+        {"p":"Like a magician who tells you how the trick is performed, STAR Collaborative’s transparent business model has exposed the staffing and consulting industry’s secrets. Here are a few examples of our transparency:"},
+        {"p":"When we work with you on a potential contracting role, we’ll tell you the client rate, so you know you’re going to get the lion’s share of the bill rate."},
+        {"p":"If we think you’re a good match, we’ll get your permission to present your resume. We never submit a resume without your knowledge and approval."},
+        {"p":"You get access to ALL of the opportunities we receive, which are instantly viewable on our website or delivered to you via email subscription."},
+        {"p":"Even though we think these things are common sense, they’re not always common practice in the industry. Our goal is to create a positive candidate experience for you based on no secrets or surprises."}
+      ]
+      },
+      {
+        "message": "Shoes",
+        "tagline": "We've been in your shoes",
+        "tagline-details": "We know who and what make a great project or team. We work to make sure you'll have both.",
+        "small-image": "assets/shoes.png",
+        "background-image": "",
+        "detail-heading": "How many staffing firms can literally say they’ve been in your shoes?",
+        "details": [
+          {"p":""}
+        ]
+        }
+]
+};
