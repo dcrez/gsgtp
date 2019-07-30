23746cf240bd5cb6f19fcc2dd862697347a98327
dcrez <danielle.creswick@gmail.com>
2019-07-29 23:35:02 -0500

    Repo cleanup

--- projects/gsgtp-meteor/imports/api/tasks.js
+++
@@ -1,2 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-export const Pulls = new Mongo.Collection('pulls');
--- projects/gsgtp-sandbox/src/components/viewEvent.vue
+++
@@ -1,7 +0,0 @@
-// viewEvent.vue
-
-<template>
-    <div class="container-fluid" style="margin-top:70px">
-        Event Details
-    </div>
-</template>
