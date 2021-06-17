41cf01282b92f3dee005dc4b9f832ec32b0bf95f
dcrez <danielle.creswick@gmail.com>
2021-06-17 09:51:32 -0500

    Populate vf

---
+++ projects/gsgtp-offline/dist/precache-manifest.e1c5206c58e3fd4bb72d7f4646c7ffb1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41723db4c7e833f3eb91",
+    "url": "/css/app.5bb7c7c3.css"
+  },
+  {
+    "revision": "a35d59596e15e8f42b44",
+    "url": "/css/chunk-vendors.d0a34ec9.css"
+  },
+  {
+    "revision": "177cade543f9cd800f20b8822ea60ede",
+    "url": "/gsgtp-rule-book-2020.pdf"
+  },
+  {
+    "revision": "217e21c587537ed1485d4e0055fc2be3",
+    "url": "/gsgtp-rule-book-2021.pdf"
+  },
+  {
+    "revision": "ad53746c3521b30b5d486b82c62914b1",
+    "url": "/img/67401982_1220469078155424_8518080179979943936_n.ad53746c.jpg"
+  },
+  {
+    "revision": "afb0432e93c31bb4299c0113381f678c",
+    "url": "/img/67899733_1220468338155498_397076933470322688_n.afb0432e.jpg"
+  },
+  {
+    "revision": "326480d3d19ed663343846941b684c55",
+    "url": "/img/IMG_56441.326480d3.jpg"
+  },
+  {
+    "revision": "7180482f6a1df97ed9437cff2be03100",
+    "url": "/img/MVIMG_20180818_160004.7180482f.jpg"
+  },
+  {
+    "revision": "cf2dcfec707e2f1352f8f07cfce3b912",
+    "url": "/img/logo.cf2dcfec.png"
+  },
+  {
+    "revision": "24f8612e093762133acfa9ba73d7976f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef3ace606e25e0342fb0",
+    "url": "/js/about.b467e6f9.js"
+  },
+  {
+    "revision": "41723db4c7e833f3eb91",
+    "url": "/js/app.6375e8e9.js"
+  },
+  {
+    "revision": "a35d59596e15e8f42b44",
+    "url": "/js/chunk-vendors.0ae96c2d.js"
+  },
+  {
+    "revision": "03488d44763c345b32dd38fc535512a5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ projects/gsgtp-offline/src/components/addHooks.vue
@@ -0,0 +1,398 @@
+<template>
+  <div>
+         <v-card :loading="performingRequest">
+         <v-container>
+           <v-card-title>
+           Add Hook
+           </v-card-title>
+           <v-alert v-if="!currentUser" color="error" dark>  
+             You must be signed in to add a hook / register for a pull.
+           </v-alert>
+           <v-card-text>
+             <v-form>
+               <!-- Find a pull -->
+               
+
+              <v-menu ref="menu" v-model="datePickerMenu" :close-on-content-click="false"
+                :return-value.sync="date" transition="scale-transition" offset-y min-width="auto">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field v-model="hookForm.pullDate" label="Choose a pull date" prepend-icon="mdi-calendar" readonly
+                    v-bind="attrs" v-on="on"></v-text-field>
+                </template>
+                <v-date-picker v-model="hookForm.pullDate" scrollable :allowed-dates="getAllowedDates(pullDates)" color="green lighten-1"
+                  event-color="blue">
+                  <v-spacer></v-spacer>
+                  <v-btn text color="primary" @click="datePickerMenu = false">
+                    Cancel
+                  </v-btn>
+                  <v-btn text color="primary" @click="$refs.menu.save(date)">
+                    OK
+                  </v-btn>
+                </v-date-picker>
+              </v-menu>
+               <div v-if="selectedPull" style="margin-top:-15px">
+                <strong>{{selectedPull.title}}</strong>, {{selectedPull.city}}, {{selectedPull.state}}
+                </div> 
+               <!-- Determine if hook is for a member -->
+               <v-switch
+                v-model="hookMember"
+                label="Member"
+                inset
+                color="warning"
+               ></v-switch>
+               <div v-if="hookMember">
+               <v-autocomplete
+                v-model="hookForm.member.selectedMember"
+                label="Select member"
+                :items="members"
+                hint="Search for primary member associated to tractor."
+                item-text="fullName"
+                item-value="userId"
+                return-object
+                chips
+                deletable-chips
+               >
+               </v-autocomplete>
+                <div v-if="hookForm.member.selectedMember != {}">
+                  123S
+               <v-autocomplete v-model="hookForm.member.vehicle"
+                label="Find vehicle"
+                :items="onlineMember"
+                hint="Select a vehicle."
+                item-text="name"
+                item-value="id"
+                return-object
+                chips
+                deletable-chips></v-autocomplete>
+                <v-autocomplete
+                v-model="hookForm.vehicleClass"
+                label="Select vehicle class"
+                :items="hookForm.member.vehicle.classes"
+                hint="Select a vehicle class"
+                item-text="name"
+                item-value="name"
+                v-if="hookForm.member.vehicle"
+                multiple
+                return-object
+                chips
+                deletable-chips
+               >
+               </v-autocomplete>
+                </div>
+               </div>
+               
+               <div v-else>
+                 <v-text-field v-model="hookForm.nonMember.vehicle" label="Vehicle" hint="Provide a tractor name."></v-text-field>
+                 <v-autocomplete
+                v-model="hookForm.vehicleClass"
+                label="Select vehicle class"
+                :items="vehicleClasses"
+                hint="Select a vehicle class"
+                item-text="name"
+                item-value="name"
+                v-if="hookForm.nonMember.vehicle"
+                multiple
+                return-object
+                chips
+                deletable-chips
+               >
+               </v-autocomplete>
+               </div>
+                            <v-text-field v-model="hookForm.pullerName" label="Puller Name" hint="Required for all guest hooks. "></v-text-field>
+
+               <v-switch v-if="userProfile.role === 'admin'"
+                v-model="hookFees"
+                inset
+                color="info"
+                label="Hook Fees"
+                >
+                </v-switch>
+                <v-alert dense outlined type="success" v-if="hookForm.successMessage != ''">{{hookForm.successMessage}}</v-alert>
+                <v-alert dense outlined type="error" v-if="hookForm.errorMessage != ''">{{hookForm.errorMessage}}</v-alert>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn text @click="dismiss(m)">Cancel</v-btn>
+                  <v-btn outlined color="success" v-if="hookMember" @click.stop="addMemberHook">Add member hook</v-btn>
+                  <v-btn outlined color="success" v-if="!hookMember" @click.stop="addGuestHook">Add guest hook</v-btn>
+                </v-card-actions>
+             </v-form>
+           </v-card-text>
+         </v-container>
+           </v-card>
+     
+  </div>
+</template>
+
+<script>
+import {db} from '../../firebaseconfig.js'
+import { mapState } from 'vuex'
+export default {
+  name: 'addHooks',
+  props: ['dateRef', 'memberId'],
+  data() {
+    return {
+      datePickerMenu: false,
+      pulls: {},
+      vehicleClasses: {},
+      vehicles: {},
+      hook: {},
+      performingRequest: false,
+      btmHook: false,
+      hookMember: false,
+      hookFees: false,
+      hookDues: '',
+      hookForm: {
+        successMessage: '',
+        errorMessage: '',
+        pullDate: '',
+        nonMember: {
+          vehicle: '',
+        },
+        member: {
+          vehicle: '',
+          selectedMember: '',
+        },
+        distance: '',
+        dq: false,
+        pull: [],
+        pullerName: '',
+        vehicleClass: ''
+      }
+    }
+  },
+  firestore() {
+    let gsgtpmember 
+    return {
+      
+      //pull: db.collection('pulls').doc(currentPull),
+      hook: db.collection('hooks').orderBy("created", "desc").limit(1),
+      members: db.collection('members'),
+      vehicles: db.collection('vehicles'),
+      vehicleClasses: db.collection('vehicleClasses').orderBy("name", "asc").where("active", "==", true),
+      pulls: db.collection('pulls').orderBy("date", "desc").where('year', '==', '2020')
+    }
+  },
+  computed: {
+    ...mapState(['currentUser','userProfile']),
+  
+    onlineMember() {
+      let hfsm = this.hookForm.member.selectedMember
+      let a = this.vehicles
+      let c
+      c = _.filter(a, function(o) {return o.member.userId == hfsm.userId})
+      console.log(c)
+      return c 
+    },
+    offlineMember() {
+      let hfsm = this.hookForm.member.selectedMember
+      let a = this.vehicles
+      let c
+      c = _.filter(a, function(o) {return o.member.memberId == hfsm.memberId})
+      console.log(c)
+      return c 
+    },
+    currentYear() {
+      var d = new Date();
+      var n = d.getFullYear();
+      return n
+    },
+    pullDates() {
+      let a = this.pulls
+      let mp = a.map(x => x.date);
+      //console.log(mp);
+      return mp
+    },
+    selectedPull() {
+      if (this.hookForm.pullDate != null) {
+        let a = this.pulls
+        let b = this.hookForm.pullDate
+        let c = _.filter(a, {'date': b})
+        let d = c.flat()
+        console.log(d)
+        return d[0]
+        
+      } 
+    },
+    duesPaid() {
+      if (this.hookForm.member.selectedMember != '') {
+      let mR = this.hookForm.member.selectedMember
+        let cD = mR.currentDues
+        let effEnd = cD.effectiveStart
+        if (mR != null && cD != null) {
+            
+            return effEnd   
+        }}
+    },
+    duesStatus() {
+      let hD = new Date(this.hookForm.pullDate)
+      let hookYear = hD.getFullYear()
+      let duesYear
+      let dis
+      if (this.duesPaid) { 
+        let dd = new Date(this.duesPaid)
+        duesYear = dd.getFullYear()
+        }
+      if (hD != null && this.duesPaid != null) {
+         if (duesYear == hookYear) {
+           return true
+         } else {return false}
+      }
+    }
+  },
+    methods: {
+      save (date) {
+        this.$refs.menu.save(date)
+      },
+      getAllowedDates(val) {
+        return val => this.pullDates.indexOf(val) !== -1
+      },
+      addMemberHook: function() {
+        this.hookForm.errorMessage = ''
+        this.performingRequest = true
+        console.log(this.selectedPull.id)
+        let form = document.getElementById("addHookForm")
+        let vn = this.hookForm.member.vehicle.name
+        let hf = this.hookForm
+        let dS = this.duesStatus
+        let pullYear = this.selectedPull.date.substring(0,4)
+        console.log('dS:'+ dS)
+        
+        // Begin class eval (create multiple hooks)
+        let eachClass = this.hookForm.vehicleClass
+          for(let i=0, len=eachClass.length; i<len; i++) {  
+            let thisClass = eachClass[i].name
+
+            db.collection('hooks').add({
+                vehicle: this.hookForm.member.vehicle.name,
+                vehicleId: this.hookForm.member.vehicle.id,
+                pull: this.selectedPull,
+                pullRef: this.selectedPull.cityRef,
+                pullId: this.selectedPull.id,
+                pullerName: this.hookForm.pullerName,
+                track: this.selectedPull.track,
+                isMember: this.hookMember,
+                member: this.hookForm.member.selectedMember,
+                duesPaid: dS,
+                dq: this.hookForm.dq,
+                vehicleClass: thisClass,
+                pullOrder: (Math.random() * 100),
+                pointsYear: pullYear,
+                created: new Date(),
+                createdBy: this.currentUser.uid
+            }).then(function(docRef) {
+                console.log("Hook written with ID: ", docRef.id)
+                hf.successMessage = "Hook(s) added successfully!"
+                hf.pullerName = '',
+                hf.isMember = true,
+                hf.vehicleClass = ''
+                hf.member.vehicle = '',
+                hf.nonMember.vehicle = ''
+                hf.member.selectedMember = '',
+                hf.hookFees = false  
+                
+            }).catch(err => {
+              this.hookForm.errorMessage = err.message
+              
+            }); 
+            this.performingRequest = false
+            }
+        },
+        addGuestHook: function() {
+        this.hookForm.errorMessage = ''
+        this.performingRequest = true
+        console.log(this.hookForm.pullerName)
+        let form = document.getElementById("addHookForm")
+        let vn = this.vehicleName
+        let hf = this.hookForm
+        let pR = this.performingRequest
+        let pullYear = this.selectedPull.date.substring(0,4)
+        
+        // Begin class eval (create multiple hooks)
+        let eachClass = this.hookForm.vehicleClass
+          for(let i=0, len=eachClass.length; i<len; i++) {  
+            let thisClass = eachClass[i].name
+
+            db.collection('hooks').add({
+                vehicle: this.hookForm.nonMember.vehicle,
+                pull: this.selectedPull.date,
+                pullRef: this.selectedPull.cityRef,
+                pullId: this.selectedPull.id,
+                pullerName: this.hookForm.pullerName,
+                track: this.selectedPull.track,
+                dq: this.hookForm.dq,
+                vehicleClass: thisClass,
+                pullOrder: (Math.random() * 100),
+                created: new Date()
+            }).then(function(docRef) {
+                console.log("Hook written with ID: ", docRef.id)
+                hf.successMessage = "Hook(s) added successfully!"
+                
+                console.log("progress: "+pR)
+                hf.pullerName = '',
+                hf.isMember = false,
+                hf.vehicleClass = ''
+                hf.member.vehicle = '',
+                hf.nonMember.vehicle = ''
+            }).catch(err => {
+              console.log(hf.errorMessage)
+              hf.errorMessage = err.message
+            }); 
+            }
+
+            this.performingRequest = false
+        },
+      addHook: function() {
+            // start transition/loading view
+           
+            let form = document.getElementById("addHookForm")
+            let vn = this.vehicleName
+            let hf = this.hookForm
+            //let doc = this.vehicleForm.name.toLowerCase().split(" ").join("-")
+
+            // add hook to Firestore
+            let pullYear = this.selectedPull.date.substring(0,4)
+            let eachClass = this.hookForm.vehicleClass
+
+            for(let i=0, len=eachClass.length; i<len; i++) {
+            
+            let thisClass = eachClass[i].name
+            
+            db.collection("hooks").add({
+                vehicleName: vn,
+                vehicleId: this.hookForm.member.vehicle.id,
+                pull: this.selectedPull.date,
+                pullRef: this.selectedPull.cityRef,
+                pullId: this.selectedPull.id,
+                pullerName: this.hookForm.pullerName,
+                track: this.selectedPull.track,
+                isMember: this.hookMember,
+                duesPaid: this.hookDues,
+                dq: this.hookForm.dq,
+                vehicleClass: thisClass,
+                pullOrder: (Math.random() * 100),
+                pointsYear: pullYear,
+                created: new Date(),
+                createdBy: this.currentUser.uid
+            }).then(function(docRef) {
+                console.log("Hook written with ID: ", docRef.id)
+                hf.successMessage = "Hook(s) added successfully!"
+                hf.pullerName = '',
+                hf.isMember = false,
+                hf.vehicleClass = ''
+                hf.member.vehicle = '',
+                hf.nonMember.vehicle = ''
+                this.performingRequest = false
+            }).catch(err => {
+              this.hookForm.errorMessage = err.message
+              this.performingRequest = false
+            });
+            }
+            
+      },
+      dismiss(m) {
+        m = ""
+      }
+    }
+  }
+
+</script>
